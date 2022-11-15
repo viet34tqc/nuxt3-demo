f59d666393700c1e59e64038401e680bce1043e3
viet34tqc <viet34tqc@gmail.com>
2022-11-15 17:21:05 +0700

    Format code

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -1,21 +1,21 @@
 <template>
-  <div class="card text-center">
-    <img :src="`${product.image}`" class="thumb" />
-    <p class="font-bold text-gray-500 m-4 truncate">{{ product.title }}</p>
-    <NuxtLink :to="`/products/${product.id}`">
-      <p class="btn my-4">View Details</p>
-    </NuxtLink>
-  </div>
+	<NuxtLink :to="`/products/${product.id}`">
+		<div class="card text-center">
+			<img :src="`${product.image}`" class="thumb" />
+			<p class="font-bold text-gray-500 m-4 truncate">{{ product.title }}</p>
+			<p class="btn my-4">View Details</p>
+		</div>
+	</NuxtLink>
 </template>
 
 <script setup>
-  const { product } = defineProps(['product'])
+const { product } = defineProps(['product']);
 </script>
 
 <style scoped>
-  .thumb {
-    max-height: 120px;
-    max-width: 70%;
-    margin: 0 auto;
-  }
+.thumb {
+	max-height: 120px;
+	max-width: 70%;
+	margin: 0 auto;
+}
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,21 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxtjs/tailwindcss'
-  ],
-  app: {
-    head: {
-      title: 'Nuxt Dojo',
-      meta: [
-        {name: 'description', content: 'Everything about Nuxt 3'}
-      ],
-      link: [
-        {rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons'}
-      ]
-    }
-  },
-  runtimeConfig: {
-    currencyKey: process.env.CURRENCY_API_KEY
-  }
-})
+	modules: ['@nuxtjs/tailwindcss'],
+	app: {
+		head: {
+			title: 'Nuxt 3 Demo',
+			meta: [{ name: 'description', content: 'Nuxt 3 Demo' }],
+			link: [
+				{
+					rel: 'stylesheet',
+					href: 'https://fonts.googleapis.com/icon?family=Material+Icons',
+				},
+			],
+		},
+	},
+	runtimeConfig: {
+		public: {
+			productsBaseUrl: 'https://fakestoreapi.com/products/',
+		},
+	},
+});
